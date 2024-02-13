1e63bbb36f40b7338b07995dcc905bc561cd7bff
mac <mac@MacBook-Air-de-mac.local>
2024-02-13 11:23:29 +0100

    fix navigation routes II

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Routes , Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
 
 // Import Routes
@@ -19,133 +19,82 @@ import AppRoute from "./routes/route";
 import Error404 from "./pages/Utility/Error404";
 import NonMemberLayout from "./routes/NonMemberLayout";
 
-//Firebase helper
-// import { initFirebaseBackend } from "./helpers/firebase_helper";
-
-// Activating fake backend
-// fakeBackend();
-
-// const firebaseConfig = {
-// 	apiKey: process.env.REACT_APP_APIKEY,
-// 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
-// 	databaseURL: process.env.REACT_APP_DATABASEURL,
-// 	projectId: process.env.REACT_APP_PROJECTID,
-// 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-// 	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-// 	appId: process.env.REACT_APP_APPID,
-// 	measurementId: process.env.REACT_APP_MEASUREMENTID,
-// };
-
-// init firebase backend
-// initFirebaseBackend(firebaseConfig);
-
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-		this.getLayout = this.getLayout.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getLayout = this.getLayout.bind(this);
+  }
 
-	/**
+  /**
    * Returns the layout
    */
-	getLayout = () => {
-		let layoutCls = VerticalLayout;
+  getLayout = () => {
+    let layoutCls = VerticalLayout;
 
-		switch (this.props.layout.layoutType) {
-			case "horizontal":
-				layoutCls = HorizontalLayout;
-				break;
-			default:
-				layoutCls = VerticalLayout;
-				break;
-		}
-		return layoutCls;
-	};
+    switch (this.props.layout.layoutType) {
+      case "horizontal":
+        layoutCls = HorizontalLayout;
+        break;
+      default:
+        layoutCls = VerticalLayout;
+        break;
+    }
+    return layoutCls;
+  };
 
-	render() {
-		const Layout = this.getLayout();
-		const userData = localStorage.getItem("authUser");
+  render() {
+    const Layout = this.getLayout();
+    const userData = localStorage.getItem("authUser");
 
-		return (
-			<React.Fragment>
-				
-				{/* <Router>
-					<Routes>
-						{publicRoutes.map((route, idx) => (
-							<AppRoute
-								path={route.path}
-								layout={NonAuthLayout}
-								component={route.component}
-								key={idx}
-								isAuthProtected={false}
-							/>
-						))}
+    return (
+      <React.Fragment>
+        <Routes>
+          {publicRoutes.map((route, idx) => (
+            <Route
+              path={route.path}
+              element={
+                <NonAuthLayout>{route.component}</NonAuthLayout>
+              }
+              key={idx}
+              exact={true}
+            />
+          ))}
 
-						{authProtectedRoutes.map((route, idx) => (
-							<AppRoute
-								path={route.path}
-								layout={Layout}
-								component={route.component}
-								key={idx}
-								isAuthProtected={true}
-							/>
-						))}
-					</Routes>
-				</Router> */}
-				<Routes basename="/app">
-					
-					{publicRoutes.map((route, idx) => (
-						<Route
-							path={route.path}
-							element={
-								<NonAuthLayout>
-									{route.component}
-								</NonAuthLayout>
-							}
-							key={idx}
-							exact={true}
-						/>
-					))}
+          {authProtectedRoutes.map((route, idx) => (
+            <Route
+              path={route.path}
+              element={
+                <AppRoute>
+                  <Layout>{route.component}</Layout>
+                </AppRoute>
+              }
+              key={idx}
+              exact={true}
+            />
+          ))}
 
-					{authProtectedRoutes.map((route, idx) => (
-						<Route
-							path={route.path}
-							element={
-								<AppRoute>
-									<Layout>{route.component}</Layout>
-								</AppRoute>
-								}
-							key={idx}
-							exact={true}
-						/>
-					))}
-
-					{nonMemberRoutes.map((route, idx) => (
-						<Route
-							path={route.path}
-							element={
-								<NonMemberLayout>
-									{route.component}
-								</NonMemberLayout>
-							}
-							key={idx}
-							exact={true}
-						/>
-					))}
-					<Route path="*" element={<Error404 />} />
-
-				</Routes>
-			</React.Fragment>
-		);
-	}
+          {nonMemberRoutes.map((route, idx) => (
+            <Route
+              path={route.path}
+              element={
+                <NonMemberLayout>{route.component}</NonMemberLayout>
+              }
+              key={idx}
+              exact={true}
+            />
+          ))}
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </React.Fragment>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-	return {
-		layout: state.Layout
-	};
+const mapStateToProps = (state) => {
+  return {
+    layout: state.Layout,
+  };
 };
-
 
 export default connect(mapStateToProps, null)(App);
